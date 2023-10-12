1f117895144018b00e005160245dea86aec63a8e
Ilia Jusoevi <iliajuso@gmail.com>
2023-10-13 00:02:39 +0400

    add client-file

---
+++ src/services/UserService.ts
@@ -0,0 +1,11 @@
+import $api from "../http";
+import { AxiosResponse } from "axios";
+import { AuthResponse } from "../models/response/AuthResponse";
+import { IUser } from "../models/response/IUser";
+
+export default class AuthService {
+    static fetchUsers(): Promise<AxiosResponse<IUser[]>>{
+        return $api.get<IUser[]>('/users',);
+    }
+   
+}
