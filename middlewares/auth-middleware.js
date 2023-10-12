adf9698941968c7892a83bfa5c6258f23441b3b3
Ilia Jusoevi <iliajuso@gmail.com>
2023-10-13 00:09:21 +0400

    add sever-folder

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,22 @@
+const ApiError = require("../exceptions/api-error");
+const tokenService = require("../service/token-service");
+module.exports = function (req, res, next) {
+    try {
+        const authorizationHeader = req.headers.authorization;
+        if (!authorizationHeader) {
+            return next(ApiError.UnauthorizedErorr());
+        }
+        const accessToken = authorizationHeader.split(' ')[1];
+        if (!accessToken) {
+            return next(ApiError.UnauthorizedErorr());
+        }
+        const userData = tokenService.validateAccessToken(accessToken);
+        if (!userData) {
+            return next(ApiError.UnauthorizedErorr());
+        }
+        req.user = userData;
+        next();
+    } catch (e) {
+        return next(ApiError.UnauthorizedErorr());
+    }
+}
